b8254cda4c69631348597b05347604cc70620e56
86180 <1094554173@qq.com>
2021-10-13 12:14:41 +0800

    small framework

--- src/main/java/mole/farm/common/farmenum/Size.java
+++ src/main/java/mole/farm/common/farmenum/Size.java
@@ -4,9 +4,11 @@ package mole.farm.common.farmenum;
  * 大小
  */
 public enum Size {
-    BIG(2),MEDIUM(1),SMALL(0);
+    BIG(2,"大"),MEDIUM(1,"中"),SMALL(0,"小");
     int number;
-    Size(int number){
+    String size;
+    Size(int number,String size){
         this.number=number;
+        this.size=size;
     }
 }
