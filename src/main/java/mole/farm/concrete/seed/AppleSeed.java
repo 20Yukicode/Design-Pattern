f28ce420898280026354431bdd1e6d1709cd87e9
86180 <1094554173@qq.com>
2021-10-13 23:20:26 +0800

    small framework

--- src/main/java/mole/farm/concrete/seed/AppleSeed.java
+++ src/main/java/mole/farm/concrete/seed/AppleSeed.java
@@ -2,11 +2,6 @@ package mole.farm.concrete.seed;
 
 import mole.farm.common.farmabstract.AbstractSeed;
 
-import java.util.List;
-
 public class AppleSeed extends AbstractSeed {
-    @Override
-    public void sowSeeds(List<AbstractSeed> seedList) {
 
-    }
 }
