a33a48882e1d5fa409d2cdab34f0d66bc3cd1331
86180 <1094554173@qq.com>
2021-10-14 00:25:29 +0800

    small framework

--- src/main/java/mole/farm/common/farmabstract/AbstractFertilizer.java
+++ src/main/java/mole/farm/common/farmabstract/AbstractFertilizer.java
@@ -6,5 +6,12 @@ import mole.farm.common.farmabstract.useless.IMole;
  * 抽象农场肥料
  */
 public abstract class AbstractFertilizer implements IMole {
+    /**
+     * 名字
+     */
     protected String name;
+    /**
+     * 产地
+     */
+    protected String originPlace;
 }
