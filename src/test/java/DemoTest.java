f0f1f6efff5643649347b53efd3f579a94e4ee80
86180 <1094554173@qq.com>
2021-10-16 14:43:10 +0800

    调整包的位置

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,15 +1,16 @@
 import moleFarm.common.other.MyException;
 import moleFarm.common.factory.FarmProductFactory;
+import moleFarm.concrete.crops.Cabbage;
+import moleFarm.concrete.crops.Wheat;
 import moleFarm.concrete.seed.RiceSeed;
 
 public class DemoTest {
     public static void main(String[] args) {
-        RiceSeed cabbage = null;
         try {
-            cabbage = FarmProductFactory.createFarmProduct("RiceSeed1");
+            RiceSeed riceSeed = FarmProductFactory.createFarmProduct("RiceSeed");
+            System.out.println(riceSeed.getName());
         } catch (MyException e) {
             System.out.println(e.getMessage());
         }
-        //System.out.println(cabbage.getColor());
     }
 }
