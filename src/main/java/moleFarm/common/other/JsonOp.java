f5797651fd9b93f83841cc2b05a9d54e78755cb2
86180 <1094554173@qq.com>
2021-10-16 13:49:27 +0800

    相对路径

--- src/main/java/moleFarm/common/other/JsonOp.java
+++ src/main/java/moleFarm/common/other/JsonOp.java
@@ -9,19 +9,18 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.List;
 
-//E:\Code\Design-Pattern\src\main\java\moleFarm\common\other\JsonOp.java
 public class JsonOp {
-    public static List<String> searchJson(FarmType farmType){
+    public static List<String> searchJson(FarmType farmType) {
         String text = farmType.getText();
-        //这里绝对路径还要修改成相对路径
-            File file=new File("E:\\Code\\Design-Pattern\\src\\main\\resources\\farm.json");
+        String jsonPath = "src/main/resources/farm.json";
+        File file = new File(jsonPath);
         String path = file.getPath();
-        List<String> list=null;
+        List<String> list = null;
         try {
-            JSONReader jsonReader=new JSONReader(new FileReader(path));
-            JSONObject o =(JSONObject) jsonReader.readObject();
-            JSONObject temp=(JSONObject)(o.get("farm"));
-            list= (List<String>) temp.get(text);
+            JSONReader jsonReader = new JSONReader(new FileReader(path));
+            JSONObject o = (JSONObject) jsonReader.readObject();
+            JSONObject temp = (JSONObject) (o.get("farm"));
+            list = (List<String>) temp.get(text);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -29,6 +28,6 @@ public class JsonOp {
     }
 
     public static void main(String[] args) {
-
+        System.out.println(searchJson(FarmType.CROPS));
     }
 }
