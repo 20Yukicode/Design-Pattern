d37dbac91041367b302e21f0c807d922708bc603
86180 <1094554173@qq.com>
2021-10-16 14:22:47 +0800

    实现工厂模式

--- src/main/java/moleFarm/concrete/factory/FarmObjectFactory.java
+++ src/main/java/moleFarm/concrete/factory/FarmObjectFactory.java
@@ -10,32 +10,29 @@ import moleFarm.concrete.seed.RiceSeed;
 import java.util.List;
 
 public  class  FarmObjectFactory {
-    static <T> T createFarmProduct(String name) {
+    final static String message="对应的产品尚未加入摩尔庄园,敬请期待......";
+    /**
+     *
+     * @param name
+     * @param <T>
+     * @return
+     * @throws MyException
+     */
+    public static <T> T createFarmProduct(String name) throws MyException {
         List<String> crops = JsonOp.searchJson(ProductType.CROPS);
         List<String> farmTool = JsonOp.searchJson(ProductType.FARM_TOOL);
         List<String> seed = JsonOp.searchJson(ProductType.SEED);
         List<String> fertilizer = JsonOp.searchJson(ProductType.FERTILIZER);
-        try {
-            if (crops.contains(name)) {
-                return (T) AbstractCropsFactory.newInstance().create(name);
-            } else if (farmTool.contains(name)) {
-                return (T) AbstractFarmToolFactory.newInstance().create(name);
-            } else if (seed.contains(name)) {
-                return (T) AbstractSeedFactory.newInstance().create(name);
-            } else if(fertilizer.contains(name)){
-                return (T) AbstractFertilizerFactory.newInstance().create(name);
-            }else{
-                return null;
-            }
+        if (crops.contains(name)) {
+            return (T) AbstractCropsFactory.newInstance().create(name);
+        } else if (farmTool.contains(name)) {
+            return (T) AbstractFarmToolFactory.newInstance().create(name);
+        } else if (seed.contains(name)) {
+            return (T) AbstractSeedFactory.newInstance().create(name);
+        } else if (fertilizer.contains(name)) {
+            return (T) AbstractFertilizerFactory.newInstance().create(name);
+        } else {
+            throw new MyException(message);
         }
-        catch (MyException e){
-            System.out.println(e.getMessage());
-        }
-        return null;
-    }
-
-    public static void main(String[] args) {
-        RiceSeed cabbage = FarmObjectFactory.createFarmProduct("RiceSeed");
-        System.out.println(cabbage.getColor());
     }
 }
--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,5 +1,15 @@
+import moleFarm.common.other.MyException;
+import moleFarm.concrete.factory.FarmObjectFactory;
+import moleFarm.concrete.seed.RiceSeed;
+
 public class DemoTest {
     public static void main(String[] args) {
-        System.out.println("design pattern test");
+        RiceSeed cabbage = null;
+        try {
+            cabbage = FarmObjectFactory.createFarmProduct("RiceSeed1");
+        } catch (MyException e) {
+            System.out.println(e.getMessage());
+        }
+        //System.out.println(cabbage.getColor());
     }
 }
