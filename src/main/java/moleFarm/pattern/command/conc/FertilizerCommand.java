88868d7d334ab7a881074b630b998f84dd792493
86180 <1094554173@qq.com>
2021-10-26 14:37:11 +0800

    .

--- src/main/java/moleFarm/pattern/command/conc/FertilizerCommand.java
+++ src/main/java/moleFarm/pattern/command/conc/FertilizerCommand.java
@@ -10,12 +10,22 @@ import moleFarm.common.product.AbstractFertilizer;
  * 仓库向商店发生请求，进货肥料
  */
 public class FertilizerCommand {
-    //命令接收方，商店
+    /**
+     * 命令接收方，商店
+     */
     private Shop shop = Shop.newInstance();
-    //命令发送方，仓库
+    /**
+     * 命令发送方，仓库
+     */
+
     private MoleFarmWarehouse moleFarmWarehouse;
 
-    //商店接收命令，为仓库买入肥料
+    /**
+     * 商店接收命令，为仓库买入肥料
+     *
+     * @param fertilizer
+     * @param num
+     */
     public void execute(AbstractFertilizer fertilizer, int num) {
         try {
             shop.buyFertilizer(fertilizer, num);
--- src/main/java/moleFarm/pattern/command/conc/SeedCommand.java
+++ src/main/java/moleFarm/pattern/command/conc/SeedCommand.java
@@ -10,12 +10,21 @@ import moleFarm.common.product.AbstractSeed;
  * 仓库发送请求，从商店进货种子
  */
 public class SeedCommand {
-    //命令接收方，商店
+    /**
+     * 命令接收方，商店
+     */
     private Shop shop = Shop.newInstance();
-    //命令发送方，仓库
+    /**
+     * 命令发送方，仓库
+     */
     private MoleFarmWarehouse moleFarmWarehouse;
 
-    //商店接收命令，为仓库买入种子
+    /**
+     * 商店接收命令，为仓库买入种子
+     *
+     * @param seed
+     * @param num
+     */
     public void execute(AbstractSeed seed, int num) {
         try {
             shop.buySeeds(seed, num);
