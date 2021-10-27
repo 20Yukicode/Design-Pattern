f90a46fce1ea862985e344d9d6d3e3ab63e025a2
ww67652 <1749113286@qq.com>
2021-10-27 11:24:25 +0800

    增加随机状态与生长周期方法

--- src/main/java/moleFarm/MoleFarmBlock.java
+++ src/main/java/moleFarm/MoleFarmBlock.java
@@ -33,6 +33,31 @@ public class MoleFarmBlock implements IFarmBlock {
      */
     private List<FarmBlockStatus> statusList = new ArrayList<>();
 
+    /**
+     * 作物生长方法
+     * 采用随机方法
+     */
+    private void growth(){
+        //随机增加生长周期(0~2)
+        Double growth = (Double)Math.floor(Math.random() * 3);
+        Integer seedStatus = getSeedStatus()+growth.intValue();
+        setSeedStatus(seedStatus>7?7:seedStatus);
+    }
+
+    /**
+     * 增加农田状态
+     * 随机
+     */
+    private void addStatus(){
+        //0~2随机增加状态，3不增加异常状态
+        Double random = (Double)Math.floor(Math.random() * 4);
+        for (FarmBlockStatus value : FarmBlockStatus.values()) {
+            if (random.intValue()  == value.ordinal()) {
+                statusList.add(value);
+            }
+        }
+    }
+
     public Shape getShape() {
         return shape;
     }
@@ -83,13 +108,18 @@ public class MoleFarmBlock implements IFarmBlock {
         String seedInfo = seed == null ? "抱歉，该农田块上暂未种植作物" : ("作物" + seed.getName());
         String statusInfo = "状态：";
         String growthInfo = "生长周期：";
+        addStatus();
         if (statusList == null || statusList.size() == 0) statusInfo = "状态：正常";
         else {
             for (FarmBlockStatus i : statusList) {
                 statusInfo += i.getText() + "\t";
             }
         }
-        if (seed != null && seedStatus != null) growthInfo += SeedStatus.getSeedStatusByNum(seedStatus).getText() + "期";
+        if (seed != null && seedStatus != null){
+            //作物随机生长
+            growth();
+            growthInfo += SeedStatus.getSeedStatusByNum(seedStatus).getText() + "期";
+        }
         System.out.println(seedInfo);
         System.out.println(statusInfo);
         System.out.println(growthInfo);
